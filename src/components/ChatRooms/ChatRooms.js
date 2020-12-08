98e98c1b3bdbaf3ba2b1ebbfa849827a7cad2e4d
Sairam Pasupuleti <sairampasupuleti.42@gmail.com>
2020-12-08 20:47:52 +0530

    .

--- src/components/ChatRooms/ChatRooms.js
+++ src/components/ChatRooms/ChatRooms.js
@@ -87,7 +87,6 @@ export default function ChatRooms({ getClickForMobileAction }) {
     </div>
   );
   function handleClick() {
-    console.log("Menu Item Cliked");
     getClickForMobileAction(true);
   }
 }
