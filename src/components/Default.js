02dd7856057e81db5c89206d904f6efea2826409
Sairam Pasupuleti <sairampasupuleti.42@gmail.com>
2021-10-01 00:19:15 +0530

    UPDATE: Default Page | Commits or actions  Fix #2
    CSS margin syntax fix

--- src/components/Default.js
+++ src/components/Default.js
@@ -11,8 +11,8 @@ function Default(props) {
       </div>
       <div className="impl__soon">
           <p> More, will be implemented in the way 🤘</p>
-          <p style="margin-top: 30px"> Build #30092021235200, <a href="https://github.com/sairampasupuleti42/sairam/commits/master" target="_blank"> Click here </a> to view recent Commits/ <a href="https://github.com/sairampasupuleti42/sairam/actions" target="_blank"> GitHub Actions </a> </p>
-          <p style="margin-top: 30px"> Get in touch on <a target="_blank" href="https://www.linkedin.com/in/sairampasupuleti">
+          <p style={{marginTop: 30 +'px'}}> Build #30092021235200, <a href="https://github.com/sairampasupuleti42/sairam/commits/master" target="_blank"> Click here </a> to view recent Commits/ <a href="https://github.com/sairampasupuleti42/sairam/actions" target="_blank"> GitHub Actions </a> </p>
+          <p style={{marginTop: 30 +'px'}}> Get in touch on <a target="_blank" href="https://www.linkedin.com/in/sairampasupuleti">
                <strong>LinkedIn</strong> </a></p>
       </div>
          
