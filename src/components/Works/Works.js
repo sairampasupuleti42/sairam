6ba4957912715dd74c7e0484939e0226503c0ce3
Sairam Pasupuleti <sairampasupuleti.42@gmail.com>
2021-09-30 22:24:17 +0530

    ADD: React JS | Tesla Clone

--- src/components/Works/Works.js
+++ src/components/Works/Works.js
@@ -1,7 +1,13 @@
 import React from "react";
 import ChatRoomArea from "./../ChatRoomArea/ChatRoomArea";
 export default function Works() {
-  
+  const data = [
+    {
+      qus: ` Tesla Website Clone`,
+      multiLine: true,
+      ans: [ `<strong>https://tesla-clone-by-sairam-zdbiar.stackblitz.io/</strong>`, ` Skills:HTML5, CSS Flex Design, React JS, React Styled Components, Redux(Incomplete)`]
+    }
+  ];
   return (
     <div>
       <ChatRoomArea/>
